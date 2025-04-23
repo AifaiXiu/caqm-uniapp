e6e3c9ba1917a4fdc5dd71dbdca092df1b80381e
linghua <xah1123801846@163.com>
2025-04-23 18:02:31 +0800

    实现了除吸怪审计和不符合项的所有功能

--- src/pages/interaction/Audit.vue
+++ src/pages/interaction/Audit.vue
@@ -1,12 +1,76 @@
 <template>
   <view class="interaction h-full">
-    <nut-button type="info" @click="navTo">新增</nut-button>
+    <scroll-view
+      scroll-y
+      refresher-enabled
+      :refresher-triggered="isRefreshing"
+      @refresherrefresh="onRefresh"
+      style="height: 100%"
+    >
+      <nut-button type="info" @click="navTo">新增</nut-button>
+      <view v-for="item in audits" :key="item.id">
+        <nut-cell-group :title="item.name">
+          <nut-button type="info" @click="deleteAction(item.id)">删除</nut-button>
+          <nut-button type="info" @click="editAction(item.id)">编辑</nut-button>
+          <nut-cell title="部门" :desc="item.dept.value"></nut-cell>
+          <nut-cell title="流程" :desc="item.process.value"></nut-cell>
+          <nut-cell title="主审人" :desc="item.mainAuditor.username"></nut-cell>
+        </nut-cell-group>
+      </view>
+    </scroll-view>
   </view>
 </template>
+
 <script setup>
+  import { ref } from 'vue'
+  import { getAudits, deleteAudit } from '../../api/modules/audit'
+
+  const audits = ref([])
+  const isRefreshing = ref(false)
+
   const navTo = () => {
     uni.navigateTo({
       url: './AuditAdd'
     })
   }
+
+  // 初始化加载
+  onMounted(() => {
+    loadAudits()
+  })
+
+  // 下拉刷新
+  const onRefresh = () => {
+    isRefreshing.value = true
+    loadAudits().finally(() => {
+      setTimeout(() => (isRefreshing.value = false), 1000)
+    })
+  }
+
+  // 加载数据
+  const loadAudits = async () => {
+    const res = await getAudits()
+    audits.value = res.data.data.content
+  }
+
+  const deleteAction = (id) => {
+    deleteAudit(id).then((res) => {
+      console.log(res, '删除成功')
+      loadAudits()
+    })
+  }
+
+  const editAction = (id) => {
+    uni.navigateTo({ url: './AuditEdit?id=' + id })
+  }
 </script>
+
+<style>
+  .interaction {
+    box-sizing: border-box;
+    padding: 16rpx;
+  }
+  scroll-view {
+    height: 100%;
+  }
+</style>
---
+++ src/pages/interaction/AuditEdit.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="interaction h-full">
+    <nut-tabs v-model="state.tab11value" type="smile" swipeable>
+      <nut-tab-pane class="important-p-0 important-px-2" title="加载刷新"></nut-tab-pane>
+      <nut-tab-pane title="Tab 3"> 这是tab页面 </nut-tab-pane>
+    </nut-tabs>
+  </view>
+</template>
+<script setup>
+  import MescrollBox from './components/mescrollBox.vue'
+  import Audit from './Audit.vue'
+  const currentAuditId = ref(0)
+  onLoad((options) => {
+    currentAuditId.value = options.id
+    console.log('路由参数ID:', currentAuditId.value)
+  })
+  const state = reactive({
+    tab11value: '0'
+  })
+</script>
+<style lang="scss" scoped>
+  :deep(.nut-tabs) {
+    height: 100%;
+    .nut-tabs__content {
+      height: calc(100% - var(--window-bottom));
+    }
+  }
+</style>
